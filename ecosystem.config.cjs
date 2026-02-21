010dc6e6a75fb4092bd43a50cc10c37a9822728a
Erick Machine <85583693+erickmachine@users.noreply.github.com>
2026-02-20 21:00:31 -0400

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,30 +1,30 @@
 module.exports = {
   apps: [
     {
-      name: 'demi-bot',
+      name: 'demibot',
       script: 'index.js',
-      node_args: '--experimental-specifier-resolution=node',
-      watch: false,
-      autorestart: true,
-      max_restarts: 10,
-      restart_delay: 5000,
-      env: {
-        NODE_ENV: 'production'
-      }
-    },
-    {
-      name: 'demi-painel',
-      script: './node_modules/next/dist/bin/next',
-      args: 'start -p 3000',
+      node_args: '--experimental-modules',
       watch: false,
       autorestart: true,
       max_restarts: 10,
       restart_delay: 5000,
       env: {
         NODE_ENV: 'production',
-        PORT: 3000
       },
-      interpreter: 'node'
-    }
-  ]
+    },
+    {
+      name: 'demibot-painel',
+      script: 'node_modules/.bin/next',
+      args: 'start -p 3000',
+      cwd: './',
+      watch: false,
+      autorestart: true,
+      max_restarts: 10,
+      restart_delay: 5000,
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+    },
+  ],
 }
